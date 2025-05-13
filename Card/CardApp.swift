a7f9f9e0d12c6fc49dd3be8cbfaa724eae99bdc6
keshew <artyom.korotckov2017@yandex.ru>
2025-05-13 15:26:36 +0500

    Initial Commit

---
+++ Card/CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardApp.swift
+//  Card
+//
+//  Created by Артём Коротков on 13.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
